c07952181b9dd91346b41675a8d43a110bf6a8fb
fe-macias <fernando.macias19@icloud.com>
2026-09-08 17:12:15 -0300

    seguir avanzando

---
+++ Autor.java
@@ -0,0 +1,21 @@
+public class Autor {
+    int id;
+    String nombre;
+    public Autor(int id , String nombre) {
+
+    }
+    public int getId() {
+        return this.id;
+    }
+    public void setId(int id) {
+        
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
